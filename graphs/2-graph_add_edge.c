d3c3c1021bd555df1e14e476fb37b9a00a5589a9
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-05-26 12:33:12 +0200

    Fix edge order: append edges; style/doc updates

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -4,7 +4,11 @@
 #include "graphs.h"
 
 /**
- * find_vertex_by_name - helper to find vertex by content
+ * find_vertex_by_name - find a vertex by its content
+ * @graph: pointer to the graph to search
+ * @name: string to match against vertex->content
+ *
+ * Return: pointer to the vertex, or NULL if not found
  */
 static vertex_t *find_vertex_by_name(const graph_t *graph, const char *name)
 {
@@ -24,24 +28,43 @@ static vertex_t *find_vertex_by_name(const graph_t *graph, const char *name)
 }
 
 /**
- * add_edge_node - add an edge node from src to dest vertex
- * Returns 1 on success, 0 on failure
+ * add_edge_node - append an edge node from src to dest
+ * @src: source vertex
+ * @dest: destination vertex
+ *
+ * Return: 1 on success, 0 on failure
  */
 static int add_edge_node(vertex_t *src, vertex_t *dest)
 {
     edge_t *e = malloc(sizeof(edge_t));
+    edge_t *cur;
 
     if (!e)
         return (0);
+
     e->dest = dest;
-    e->next = src->edges;
-    src->edges = e;
+    e->next = NULL;
+    if (!src->edges)
+        src->edges = e;
+    else
+    {
+        cur = src->edges;
+        while (cur->next)
+            cur = cur->next;
+        cur->next = e;
+    }
     src->nb_edges++;
     return (1);
 }
 
 /**
  * graph_add_edge - add edge(s) between two vertices identified by name
+ * @graph: pointer to the graph
+ * @src: name of source vertex
+ * @dest: name of destination vertex
+ * @type: edge type (UNIDIRECTIONAL or BIDIRECTIONAL)
+ *
+ * Return: 1 on success, 0 on failure
  */
 int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
 {
@@ -55,16 +78,28 @@ int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_
     if (!v_src || !v_dest)
         return (0);
 
+    /* add src -> dest */
     if (!add_edge_node(v_src, v_dest))
         return (0);
 
+    /* if bidirectional, add dest -> src; rollback on failure */
     if (type == BIDIRECTIONAL)
     {
         if (!add_edge_node(v_dest, v_src))
         {
-            /* rollback */
-            edge_t *tmp = v_src->edges;
-            v_src->edges = tmp->next;
+            /* rollback last edge added to v_src */
+            edge_t *tmp = v_src->edges, *prev = NULL;
+            if (!tmp)
+                return (0);
+            while (tmp->next)
+            {
+                prev = tmp;
+                tmp = tmp->next;
+            }
+            if (prev)
+                prev->next = NULL;
+            else
+                v_src->edges = NULL;
             free(tmp);
             v_src->nb_edges--;
             return (0);
