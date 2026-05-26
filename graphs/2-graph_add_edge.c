cc3fb7a7d529f761b01456ce95446b3e07e2d319
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-05-26 12:47:12 +0200

    Style: fix tabs/leading-space in 2-graph_add_edge.c

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -12,19 +12,19 @@
  */
 static vertex_t *find_vertex_by_name(const graph_t *graph, const char *name)
 {
-    vertex_t *v;
+	vertex_t *v;
 
-    if (!graph || !name)
-        return (NULL);
+	if (!graph || !name)
+		return (NULL);
 
-    v = graph->vertices;
-    while (v)
-    {
-        if (v->content && strcmp(v->content, name) == 0)
-            return (v);
-        v = v->next;
-    }
-    return (NULL);
+	v = graph->vertices;
+	while (v)
+	{
+		if (v->content && strcmp(v->content, name) == 0)
+			return (v);
+		v = v->next;
+	}
+	return (NULL);
 }
 
 /**
@@ -36,25 +36,25 @@ static vertex_t *find_vertex_by_name(const graph_t *graph, const char *name)
  */
 static int add_edge_node(vertex_t *src, vertex_t *dest)
 {
-    edge_t *e = malloc(sizeof(edge_t));
-    edge_t *cur;
+	edge_t *e = malloc(sizeof(edge_t));
+	edge_t *cur;
 
-    if (!e)
-        return (0);
+	if (!e)
+		return (0);
 
-    e->dest = dest;
-    e->next = NULL;
-    if (!src->edges)
-        src->edges = e;
-    else
-    {
-        cur = src->edges;
-        while (cur->next)
-            cur = cur->next;
-        cur->next = e;
-    }
-    src->nb_edges++;
-    return (1);
+	e->dest = dest;
+	e->next = NULL;
+	if (!src->edges)
+		src->edges = e;
+	else
+	{
+		cur = src->edges;
+		while (cur->next)
+			cur = cur->next;
+		cur->next = e;
+	}
+	src->nb_edges++;
+	return (1);
 }
 
 /**
@@ -68,43 +68,43 @@ static int add_edge_node(vertex_t *src, vertex_t *dest)
  */
 int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
 {
-    vertex_t *v_src, *v_dest;
+	vertex_t *v_src, *v_dest;
 
-    if (!graph || !src || !dest)
-        return (0);
+	if (!graph || !src || !dest)
+		return (0);
 
-    v_src = find_vertex_by_name(graph, src);
-    v_dest = find_vertex_by_name(graph, dest);
-    if (!v_src || !v_dest)
-        return (0);
+	v_src = find_vertex_by_name(graph, src);
+	v_dest = find_vertex_by_name(graph, dest);
+	if (!v_src || !v_dest)
+		return (0);
 
-    /* add src -> dest */
-    if (!add_edge_node(v_src, v_dest))
-        return (0);
+	/* add src -> dest */
+	if (!add_edge_node(v_src, v_dest))
+		return (0);
 
-    /* if bidirectional, add dest -> src; rollback on failure */
-    if (type == BIDIRECTIONAL)
-    {
-        if (!add_edge_node(v_dest, v_src))
-        {
-            /* rollback last edge added to v_src */
-            edge_t *tmp = v_src->edges, *prev = NULL;
-            if (!tmp)
-                return (0);
-            while (tmp->next)
-            {
-                prev = tmp;
-                tmp = tmp->next;
-            }
-            if (prev)
-                prev->next = NULL;
-            else
-                v_src->edges = NULL;
-            free(tmp);
-            v_src->nb_edges--;
-            return (0);
-        }
-    }
+	/* if bidirectional, add dest -> src; rollback on failure */
+	if (type == BIDIRECTIONAL)
+	{
+		if (!add_edge_node(v_dest, v_src))
+		{
+			/* rollback last edge added to v_src */
+			edge_t *tmp = v_src->edges, *prev = NULL;
+			if (!tmp)
+				return (0);
+			while (tmp->next)
+			{
+				prev = tmp;
+				tmp = tmp->next;
+			}
+			if (prev)
+				prev->next = NULL;
+			else
+				v_src->edges = NULL;
+			free(tmp);
+			v_src->nb_edges--;
+			return (0);
+		}
+	}
 
-    return (1);
+	return (1);
 }
