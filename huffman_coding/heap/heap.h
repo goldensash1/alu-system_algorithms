82eebc09cbe9c3ea2c1a41cdc8b3d885a73f23f7
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-05-27 07:39:48 +0200

    Docs: add heap struct descriptions

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct binary_tree_node_s - Binary tree node data structure
+ *
+ * @data: Data stored in a node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ * @parent: Pointer to the parent node
+ */
 typedef struct binary_tree_node_s
 {
 	void *data;
@@ -11,6 +19,13 @@ typedef struct binary_tree_node_s
 	struct binary_tree_node_s *parent;
 } binary_tree_node_t;
 
+/**
+ * struct heap_s - Heap data structure
+ *
+ * @size: Size of the heap (number of nodes)
+ * @data_cmp: Function to compare two nodes data
+ * @root: Pointer to the root node of the heap
+ */
 typedef struct heap_s
 {
 	size_t size;
